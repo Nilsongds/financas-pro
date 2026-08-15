41db737d2430a5f284d3d2f5be570e4bb063a23c
Nilsongds <nilsongds@gmail.com>
2026-08-15 10:36:34 -0300

    Add Android app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.nilsongds.financaspro',
+  appName: 'Finanças Pro',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
